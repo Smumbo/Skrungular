d0eb1e6bc9cf683fb4a1f9b8fad96d54adac25da
Jade <jhmarmo@charter.net>
2020-02-01 16:11:46 -0700

    making 4 dotted lines

--- Assets/KunalTandon/DottedLine/Scripts/DottedLineDemo.cs
+++ Assets/KunalTandon/DottedLine/Scripts/DottedLineDemo.cs
@@ -10,13 +10,26 @@ public class DottedLineDemo : MonoBehaviour
     [Header("Points for line 2")]
     public Vector2 pointX;
     public Vector2 pointY;
+    [Header("Points for line 2")]
+    public Vector2 pointC;
+    public Vector2 pointD;
+    [Header("Points for line 2")]
+    public Vector2 pointE;
+    public Vector2 pointF;
+
     // Update is called once per frame
     void Update()
     {
-        //Creating a dotted line
+        //Creating a dotted line 1
         DottedLine.DottedLine.Instance.DrawDottedLine(pointA, pointB);
 
-        //Creating another dotted line
+        //Creating another dotted line 2
         DottedLine.DottedLine.Instance.DrawDottedLine(pointX, pointY);
+
+        //Creating another dotted line 2
+        DottedLine.DottedLine.Instance.DrawDottedLine(pointC, pointD);
+
+        //Creating another dotted line 2
+        DottedLine.DottedLine.Instance.DrawDottedLine(pointE, pointF);
     }
 }
